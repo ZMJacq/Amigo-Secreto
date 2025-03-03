0c42097bc57ddba23f5acf15d2e37e47b2ca25a1
ZM <zarek03@hotmail.com>
2025-03-02 19:11:47 -0600

    Se agregan funciones de mostrar texto y guardarlo

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array de los amigos
 let listaAmigos = [];
 
 function agregarAmigo() {
     let agregarAmigo = document.getElementById("amigo");
-    let Amigos = agregarAmigo.value;
+    let amigos = agregarAmigo.value;
 
-    // También se puede colocar un =="" ó un == False
-    if (!Amigos) {
+    // También se puede colocar un =="" ó un == False para negar la funcion, en este caso, si no temgo.
+    if (!amigos) {
         alert("Ingresa el nombre de un amigo");
         return;
     }
-    listaAmigos.push(Amigos);
+    listaAmigos.push(amigos);
     agregarAmigo.value = "";
-    
+    mostrarAmigo()
 };
+
+function mostrarAmigo(){
+    let amigosEnLista = document.getElementById("listaAmigos");
+    // Limpia la lista de amigos.
+    amigosEnLista.innerHTML = "";
+    
+    for ( let i = 0; i < listaAmigos.length; i++ ) {
+        let item = document.createElement("li");
+        item.textContent = listaAmigos[i];
+        amigosEnLista.appendChild(item);
+    }
+}
